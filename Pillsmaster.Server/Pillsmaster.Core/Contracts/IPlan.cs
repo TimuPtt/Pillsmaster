a929a9871c22f1228cf54883d35d00ad29994a0c
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-15 17:37:21 +0300

    Implement contracts

---
+++ Pillsmaster.Server/Pillsmaster.Core/Contracts/IPlan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pillsmaster.Core.Models;
+
+namespace Pillsmaster.Core.Contracts
+{
+    internal interface IPlan
+    {
+        void TakeMedicine();
+
+        void AddTakes(List<Take> takes);
+    }
+}
