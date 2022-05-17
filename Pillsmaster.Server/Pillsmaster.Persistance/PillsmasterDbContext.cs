36510966593754f32734e1d1240a865602b84ca7
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-18 00:30:45 +0300

    Cerate persistense layer and dbContext

---
+++ Pillsmaster.Server/Pillsmaster.Persistance/PillsmasterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pillsmaster.Application.Interfaces;
+using Pillsmaster.Domain.Models;
+
+
+namespace Pillsmaster.Persistence
+{
+    public class PillsmasterDbContext : DbContext, IPillsmasterDbContext
+    {
+        public DbSet<MedicationDay> MedicationDays { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<UserMedicine> UserMedicines { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Take> Takes { get; set; }
+
+        public PillsmasterDbContext(DbContextOptions<PillsmasterDbContext> options) : base(options) { }
+
+    }
+}
