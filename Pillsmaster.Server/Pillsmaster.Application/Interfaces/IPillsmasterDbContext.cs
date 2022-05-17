61b4affd9cafbc2932bf10af59f8ebae0ec666a7
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-18 00:33:22 +0300

    Implement db context interface

---
+++ Pillsmaster.Server/Pillsmaster.Application/Interfaces/IPillsmasterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.Interfaces
+{
+    public interface IPillsmasterDbContext
+    {
+        DbSet<MedicationDay> MedicationDays { get; set; }
+        DbSet<Medicine> Medicines { get; set; }
+        DbSet<UserMedicine> UserMedicines { get; set; }
+        DbSet<Plan> Plans { get; set; }
+        DbSet<Take> Takes { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+
+    }
+}
