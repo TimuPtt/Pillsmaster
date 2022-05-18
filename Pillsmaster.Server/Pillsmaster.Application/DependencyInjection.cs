a3f0f18e37797f220725f4245ec3cb0ee452baa8
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-18 19:53:42 +0300

    Add dependency injection calsses

---
+++ Pillsmaster.Server/Pillsmaster.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pillsmaster.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicator(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
