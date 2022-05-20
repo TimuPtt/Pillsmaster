1d610ec67e59cd46f62fa4da46d3ef637752d6ec
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-20 20:03:52 +0300

    Implement Medicine service
    Implement CreateMedicine method

---
+++ Pillsmaster.Server/Pillsmaster.Application/Services/MedicineService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pillsmaster.Application.Interfaces;
+using Pillsmaster.Application.ViewModels;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.Services
+{
+    public class MedicineService : BaseService, IMedicineService
+    {
+        public MedicineService(IPillsmasterDbContext dbContext) : base(dbContext) { }
+
+        public async Task<Guid> CreateMedicine(MedicineViewModel medicineVm)
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
+            var medicine = new Medicine()
+            {
+                MedicineId = Guid.NewGuid(),
+                TradeName = medicineVm.TradeName,
+                InternationalName = medicineVm.InternationalName,
+                PharmaType = medicineVm.PharmaType,
+                ActiveIngredientCount = medicineVm.ActiveIngredientCount
+            };
+
+            await _dbContext.Medicines.AddAsync(medicine, token); 
+            await _dbContext.SaveChangesAsync(token).ConfigureAwait(false);
+
+            return medicine.MedicineId;
+        }
+
+        public async Task<List<Medicine>> ReadMedicinesByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Medicine> ReadMedicineById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateMedicine(Medicine medicine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteMedicine(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ Pillsmaster.Server/Pillsmaster.Application/ViewModels/MedicineViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pillsmaster.Application.ViewModels
+{
+    public class MedicineViewModel
+    {
+        public string TradeName { get; set; }
+
+        public string InternationalName { get; set; } = string.Empty;
+
+        public string PharmaType { get; set; } = string.Empty;
+
+        public int ActiveIngredientCount { get; set; }
+    }
+}
