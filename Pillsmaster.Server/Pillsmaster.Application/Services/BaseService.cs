979f54274d1688d515d4819ec98731a8e986759e
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-20 20:02:41 +0300

    Implement base service

---
+++ Pillsmaster.Server/Pillsmaster.Application/Services/BaseService.cs
@@ -0,0 +1,12 @@
+﻿using Pillsmaster.Application.Interfaces;
+
+namespace Pillsmaster.Application.Services
+{
+    public abstract class BaseService
+    {
+        private protected readonly IPillsmasterDbContext _dbContext;
+
+        public BaseService(IPillsmasterDbContext dbContext) =>
+            _dbContext = dbContext;
+    }
+}
