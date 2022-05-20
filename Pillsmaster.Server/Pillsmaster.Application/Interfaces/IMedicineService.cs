96ce71ae9f31304e927ff2fee02b684aa385d364
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-20 20:04:16 +0300

    Implement IMedicineService inteface

---
+++ Pillsmaster.Server/Pillsmaster.Application/Interfaces/IMedicineService.cs
@@ -0,0 +1,14 @@
+﻿using Pillsmaster.Application.ViewModels;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.Interfaces
+{
+    internal interface IMedicineService
+    {
+        Task<Guid> CreateMedicine(MedicineViewModel medicine);
+        Task<List<Medicine>> ReadMedicinesByName(string name);
+        Task<Medicine> ReadMedicineById(Guid id);
+        Task UpdateMedicine(Medicine medicine);
+        Task DeleteMedicine(Guid id);
+    }
+}
