3bf22d9926969837dca29493ba1baeaec286bf12
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-26 21:44:28 +0300

    Implement UserMedicineService interface

---
+++ Pillsmaster.Server/Pillsmaster.Application/Interfaces/IUserMedicineService.cs
@@ -0,0 +1,16 @@
+﻿using Pillsmaster.Application.ViewModels;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.Interfaces
+{
+    public interface IUserMedicineService
+    {
+        Task<Guid> CreateUserMedicine(UserMedicineViewModel userMedicineVm, CancellationToken cancellationToken);
+        Task<List<UserMedicine>> ReadUserMedicines(Guid userId, CancellationToken cancellationToken);
+
+        Task UpdateUserMedicine(Guid userMedicineId, UserMedicineViewModel userMedicineVm,
+            CancellationToken cancellationToken);
+
+        Task DeleteUserMedicine(Guid userMedicineId, CancellationToken cancellationToken);
+    }
+}
