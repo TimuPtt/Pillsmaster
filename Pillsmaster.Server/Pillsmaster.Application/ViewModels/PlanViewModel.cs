702efae70535de7e5461f45e6d971d0650b73458
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-05-26 21:45:23 +0300

    Implement View Models

---
+++ Pillsmaster.Server/Pillsmaster.Application/ViewModels/PlanViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.ViewModels
+{
+    public class PlanViewModel
+    {
+        public Guid UserId { get; set; }
+
+        public int MedicineCount { get; set; }
+
+        public int Duration { get; set; }
+
+        public bool IsEnoughToFinish { get; set; }
+
+        public string? FoodStatus { get; set; }
+
+        public bool IsFoodDependent { get; set; }
+
+        public string? PlanStatus { get; set; }
+
+        public Guid MedicationDayId { get; set; }
+
+        public DateTime? LastTakeTime { get; set; }
+
+        public Queue<Take> Takes { get; set; }
+    }
+}
---
+++ Pillsmaster.Server/Pillsmaster.Application/ViewModels/UserMedicineViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.ViewModels
+{
+    public class UserMedicineViewModel
+    {
+        public Guid UserId { get; set; }
+
+        public Guid UserPlanId { get; set; }
+
+        public Guid MedicineId { get; set; }
+
+        public Medicine? Medicine { get; set; }
+    }
+}
