dc733b5a7d9694082365ccb80f3fbdd7cfcf4980
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:10:06 +0300

    Implement DI container for application level

---
+++ src/Pillsmaster.Server/Pillsmaster.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pillsmaster.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        return services.AddMediatR(assembly);
+    }
+}
