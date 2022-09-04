c6ddef44a9fc2d7acb134d58120e7fbcc09e99f9
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:15:48 +0300

    Create Api-level dtos

---
+++ src/Pillsmaster.Server/Pillsmaster.Server/Dtos/CreatePlanDto.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Pillsmaster.Application.Common.Mappings;
+using Pillsmaster.Application.Plans.Commands.CreatePlan;
+
+namespace Pillsmaster.API.Dtos;
+
+public class CreatePlanDto : IMapWith<CreatePlanCommand>
+{
+    public Guid UserMedicineId { get; set; }
+    public int MedicineCount { get; set; }
+    public int Duration { get; set; }
+    public bool IsEnoughToFinish { get; set; }
+    public int FoodStatusId { get; set; }
+    public bool IsFoodDependent { get; set; }
+    public int PlanStatusId { get; set; }
+    public MedicationDayDto? MedicationDay { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? NextTakeTime { get; set; }
+    public List<TakeDto>? Takes { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<CreatePlanDto, CreatePlanCommand>();
+    }
+}
+
---
+++ src/Pillsmaster.Server/Pillsmaster.Server/Dtos/CreateUserMedicineDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Pillsmaster.Application.Common.Mappings;
+using Pillsmaster.Application.UserMedicines.Commands.CreateUserMedicine;
+
+namespace Pillsmaster.API.Dtos;
+
+public class CreateUserMedicineDto : IMapWith<CreateUserMedicineCommand>
+{
+    public string TradeName { get; set; }
+    public string InternationalName { get; set; }
+    public int PharmaTypeId { get; set; }
+    public int ActiveIngredientCount { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<CreateUserMedicineDto, CreateUserMedicineCommand>();
+    }
+}
