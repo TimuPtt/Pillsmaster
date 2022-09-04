b9bb6eb8b1fdc87336488db421888666402aa713
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:12:57 +0300

    Update UserMedicineViewModel.cs

--- src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/UserMedicineViewModel.cs
+++ src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/UserMedicineViewModel.cs
@@ -1,11 +1,24 @@
-﻿using Pillsmaster.Domain.Models;
+﻿using AutoMapper;
+
+using Pillsmaster.Application.Common.Mappings;
+using Pillsmaster.Domain.Models;
 
 namespace Pillsmaster.Application.ViewModels
 {
-    public class UserMedicineViewModel
+    public class UserMedicineViewModel : IMapWith<UserMedicine>
     {
-        public Guid UserPlanId { get; set; }
+        public Guid Id { get; set; }
+        public string TradeName { get; set; }
+        public string InternationalName { get; set; }
+        public string PharmaType { get; set; }
+        public int ActiveIngredientCount { get; set; }
 
-        public Guid MedicineId { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UserMedicine, UserMedicineViewModel>()
+                .ForMember(userMedicineVm => userMedicineVm.PharmaType,
+                    opt =>
+                        opt.MapFrom(medicine => medicine.PharmaType.Type));
+        }
     }
 }
