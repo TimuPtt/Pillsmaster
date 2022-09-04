1d886ed3dfdf6f45a714cea20da1907dd39c9dd9
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:12:49 +0300

    Update PlanViewModel.cs

--- src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/PlanViewModel.cs
+++ src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/PlanViewModel.cs
@@ -1,29 +1,35 @@
-﻿using Pillsmaster.Domain.Models;
+﻿using AutoMapper;
+
+using Pillsmaster.Application.Common.Mappings;
+using Pillsmaster.Domain.Models;
 
 namespace Pillsmaster.Application.ViewModels
 {
-    public class PlanViewModel
+    public class PlanViewModel : IMapWith<Plan>
     {
+        public Guid Id { get; set; }
         public int MedicineCount { get; set; }
-
         public int Duration { get; set; }
-
         public bool IsEnoughToFinish { get; set; }
-
         public string? FoodStatus { get; set; }
-
         public bool IsFoodDependent { get; set; }
-
         public string? PlanStatus { get; set; }
-
         public int TakesCount { get; set; }
-
-        public MedicationDayViewModel MedicationDay { get; set; }
-
+        public MedicationDayViewModel? MedicationDay{ get; set; }
+        public UserMedicineViewModel? UserMedicine { get; set; }
         public DateTime StartDate { get; set; }
-
         public DateTime? NextTakeTime { get; set; }
-
-        public List<TakeViewModel> Takes { get; set; }
+        public List<TakeViewModel>? Takes { get; set; }
+        
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Plan, PlanViewModel>()
+                .ForMember(planVm => planVm.FoodStatus,
+                    opt =>
+                        opt.MapFrom(plan => plan.FoodStatus!.Status))
+                .ForMember(planVm => planVm.PlanStatus,
+                    opt =>
+                        opt.MapFrom(plan => plan.PlanStatus!.Status));
+        }
     }
 }
