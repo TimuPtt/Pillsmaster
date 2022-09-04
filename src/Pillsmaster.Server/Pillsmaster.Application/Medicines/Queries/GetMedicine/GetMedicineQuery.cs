c94aab6aaf681f94e1c9702076275eacd364977f
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:10:47 +0300

    Implement medicine command and queries

---
+++ src/Pillsmaster.Server/Pillsmaster.Application/Medicines/Queries/GetMedicine/GetMedicineQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Pillsmaster.Application.ViewModels;
+
+namespace Pillsmaster.Application.Medicines.Queries.GetMedicine;
+
+public class GetMedicineQuery : IRequest<MedicineViewModel>
+{
+    public string TradeName { get; set; }
+}
---
+++ src/Pillsmaster.Server/Pillsmaster.Application/Medicines/Queries/GetMedicine/GetMedicineQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using Pillsmaster.Application.Common;
+using Pillsmaster.Application.Common.Exceptions;
+using Pillsmaster.Application.Interfaces;
+using Pillsmaster.Application.ViewModels;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.Medicines.Queries.GetMedicine;
+
+public class GetMedicineQueryHandler : BaseQueryHandler,
+    IRequestHandler<GetMedicineQuery, MedicineViewModel>
+{
+    public GetMedicineQueryHandler(IPillsmasterDbContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
+
+    public async Task<MedicineViewModel> Handle(GetMedicineQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await _dbContext.Medicines
+            .FirstOrDefaultAsync(medicine =>
+                medicine.TradeName == request.TradeName, cancellationToken);
+
+        if (entity == null)
+            throw new NotFoundException(typeof(Medicine), request.TradeName);
+
+        return _mapper.Map<MedicineViewModel>(entity);
+    }
+}
