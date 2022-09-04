6f0ba52100e06364e8c98d693d10840014b4dd00
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:13:32 +0300

    Update Plan.cs

--- src/Pillsmaster.Server/Pillsmaster.Core/Models/Plan.cs
+++ src/Pillsmaster.Server/Pillsmaster.Core/Models/Plan.cs
@@ -1,35 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace Pillsmaster.Domain.Models
 {
     public class Plan
     {
         public Guid Id { get; set; }
-
         public Guid UserId { get; set; }
-
-        public int MedicineCount { get; set; }
-
-        public int Duration { get; set; }
-
-        public bool IsEnoughToFinish { get; set; }
-
-        public string? FoodStatus { get; set; }
-
-        public bool IsFoodDependent { get; set; }
-
-        public string? PlanStatus { get; set; }
-
-        public int TakesCount { get; set; }
-
+        public Guid UserMedicineId { get; set; }
         public Guid MedicationDayId { get; set; }
-
-        public MedicationDay MedicationDay { get; set; }
-
+        public int MedicineCount { get; set; }
+        public int Duration { get; set; }
+        public bool IsEnoughToFinish { get; set; }
+        public int FoodStatusId { get; set; }
+        public FoodStatus? FoodStatus { get; set; }
+        public bool IsFoodDependent { get; set; }
+        public int PlanStatusId { get; set; }
+        public PlanStatus? PlanStatus { get; set; }
+        public int TakesCount { get; set; }
+        public UserMedicine? UserMedicine { get; set; }
+        public MedicationDay? MedicationDay { get; set; }
         public DateTime StartDate { get; set; }
-
         public DateTime? NextTakeTime { get; set; }
-
-        public List<Take>? Takes { get; set; } 
+        public List<Take>? Takes { get; set; }
     }
 }
