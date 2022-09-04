25ac3dc63cae6b9bbf179b7b2214c955d41ebe83
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-04 21:12:53 +0300

    Update TakeViewModel.cs

--- src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/TakeViewModel.cs
+++ src/Pillsmaster.Server/Pillsmaster.Application/ViewModels/TakeViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace Pillsmaster.Application.ViewModels
+﻿using AutoMapper;
+using Pillsmaster.Application.Common.Mappings;
+using Pillsmaster.Domain.Models;
+
+namespace Pillsmaster.Application.ViewModels
 {
-    public class TakeViewModel
+    public class TakeViewModel : IMapWith<Take>
     {
         public DateTime TakeTime { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Take, TakeViewModel>();
+        }
     }
 }
