9802b0ea5245f63be26f8ecab005a8738c2b671b
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2022-09-08 20:06:13 +0300

    Refactoring

--- src/Pillsmaster.Server/Pillsmaster.Application/UserMedicines/Queries/GetUserMedicines/GetUserMedicinesQuery.cs
+++ src/Pillsmaster.Server/Pillsmaster.Application/UserMedicines/Queries/GetUserMedicines/GetUserMedicinesQuery.cs
@@ -4,7 +4,7 @@ using Pillsmaster.Domain.Models;
 
 namespace Pillsmaster.Application.UserMedicines.Queries.GetUserMedicines;
 
-public class GetUserMedicineQuery : IRequest<IEnumerable<UserMedicineViewModel>>
+public class GetUserMedicinesQuery : IRequest<IEnumerable<UserMedicineViewModel>>
 {
     public Guid UserId { get; set; }
 }
